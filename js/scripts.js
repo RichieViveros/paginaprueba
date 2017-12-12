081c0e1130e3ec574679b0bf156c26f5cf756355
Rviveros <jviveros@gabinete.mx>
2017-12-12 12:32:57 -0600

    Sitio basico construido

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//*********Botón menu "toggle de clases"*********
+
 var btnMenu = document.getElementById('btn-menu');
 var nav = document.getElementById('navega');
 
@@ -7,3 +9,25 @@ btnMenu.addEventListener('click', function(){
 	//al nav se le agrega la clase "mostrar"
 	nav.classList.toggle('mostrar');
 })
+
+//********* Animación Pila*********
+
+function chargebattery() {
+  var a;
+  a = document.getElementById("div1");
+  a.innerHTML = "&#xf244;";
+  setTimeout(function () {
+      a.innerHTML = "&#xf243;";
+    }, 1000);
+  setTimeout(function () {
+      a.innerHTML = "&#xf242;";
+    }, 2000);
+  setTimeout(function () {
+      a.innerHTML = "&#xf241;";
+    }, 3000);
+  setTimeout(function () {
+      a.innerHTML = "&#xf240;";
+    }, 4000);
+}
+chargebattery();
+setInterval(chargebattery, 5000);
